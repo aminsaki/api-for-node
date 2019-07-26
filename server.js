601c9bdc0d1f04726caa05cc7ea9398b06f164c3
aminsaki69 <aminsaki69@gmail.com>
2019-07-26 19:35:19 +0430

    new prorger a test

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const app = require('./app');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
+
+
+
+
+
+
+
+
