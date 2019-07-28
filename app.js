d6235f0a1feff9066f916fc98988a692ceeb8223
aminsaki69 <aminsaki69@gmail.com>
2019-07-28 23:54:03 +0430

    day function erro 404 and 500 wirte code

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
-const   app  = express();
+const app = express();
 const morgan = require('morgan');
 
 app.use(morgan('dev'));
 
 
-const  productRoutrs = require('./api/routes/products');
-const  orderRoutrs = require('./api/routes/orders');
+const productRoutrs = require('./api/routes/products');
+const orderRoutrs = require('./api/routes/orders');
 
-app.use('/products',productRoutrs);
-app.use('/orders',orderRoutrs);
+app.use('/products', productRoutrs);
+app.use('/orders', orderRoutrs);
+
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status();
+    res.status(404).send({error: {message: error.message}})
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(500).send({error: {message: error.message}})
+});
+
 
 module.exports = app;
